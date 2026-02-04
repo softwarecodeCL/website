8541e36775344da8a5fa58ee3939d24d2c822a74
Marco Estay <m.estayroman@gmail.com>
2026-02-04 13:34:20 -0300

    json d

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,20 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+  // @ts-check
+  import { defineConfig } from 'astro/config';
 
-import mdx from '@astrojs/mdx'
-import node from '@astrojs/node';
-import sitemap from '@astrojs/sitemap';
+  import mdx from '@astrojs/mdx'
+  import node from '@astrojs/node';
+  import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
-export default defineConfig({
-    site: 'https://www.softwarecode.cl',  
-    output: 'server', // Habilitar SSR
-    adapter: node({
-      mode: 'standalone', // Genera un servidor independiente listo para producción
-    }),
-    root: './',         // Raíz del proyecto (opcional si ya es la raíz)
-    srcDir: './src',    // Define que los archivos fuente están en `src/`
-    publicDir: './public', // Define que los archivos estáticos están en `public/`
-    outDir: './dist',   // Carpeta de salida tras el build
-    integrations: [mdx(), sitemap()],
-}); 
+  // https://astro.build/config
+  export default defineConfig({
+      site: 'https://www.softwarecode.cl',  
+      output: 'server', // Habilitar SSR
+      adapter: node({
+        mode: 'standalone', // Genera un servidor independiente listo para producción
+      }),
+      root: './',         // Raíz del proyecto (opcional si ya es la raíz)
+      srcDir: './src',    // Define que los archivos fuente están en `src/`
+      publicDir: './public', // Define que los archivos estáticos están en `public/`
+      outDir: './dist',   // Carpeta de salida tras el build
+      integrations: [mdx(), sitemap()],
+  }); 
